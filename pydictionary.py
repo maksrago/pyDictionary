cba2f32e265cc03e9cc9042e78e4515e2860de66
Maks <maxrago123@gmail.com>
2019-04-05 22:33:52 -0400

    - Fixed API calls for words that have an index less than 3 (really 2 because index starts at 0) - Made printing of definitions much more efficient - When a new word is looked up the textbox actually clears now

--- pydictionary.py
+++ pydictionary.py
@@ -5,39 +5,47 @@ from tkinter import messagebox
 root = Tk()
 
 #modify root window
-root.title("Simple Dictionary")
+root.title("pyDictionary")
 root.geometry("330x200")
 
 app = Frame(root)
 app.grid()
-
 label = Label(app, text = "Lookup: ")
 label.grid()
-
 textEntry = Entry(app)
 textEntry.grid()
-
 textOutput = Text(app, height=8, width=40)
 textOutput.grid()
 
-
 def retrieveUserInput():
-#    textOutput.delete('1.0')
- #   textOutput.update()
     print(textEntry.get())
     userWord = textEntry.get().lower()
     checkWord(userWord)
 
+#checks word validity and prints the index, max of 3 definitions
 def checkWord(recievedWord):
+    textOutput.delete('1.0', END)
+
     payload = {'sp' : recievedWord, 'md' : 'd'}
     r = requests.get('https://api.datamuse.com/words', params=payload)
     apiReturn = r.json()[0]['word']
 
     if recievedWord == apiReturn:
-        print("Good")
-        definiton = r.json()[0]['defs'][0]
-        textOutput.insert('1.0', definiton)
-        
+        print("Word exists within the API.")
+        indexSize = len(r.json()[0]['defs'])
+        print(indexSize)
+        i = 0;
+        while indexSize >= 0:
+            textOutput.insert('1.0', r.json()[0]['defs'][i] + '\n')
+            if indexSize == 0:
+                break
+            elif indexSize == 1:
+                break
+            elif i == 2:
+                break
+            i += 1
+
+    #Error message in case of word not being recognized or being unable to connect to server
     else:
         print('Error')
         messagebox.showinfo('Error', 'Please check your connection or ' + recievedWord + ' is a valid word.')
@@ -48,7 +56,3 @@ button.grid()
 
 #initialize the event loop
 root.mainloop()
-
-#todo
-#fix error handeling
-#fix ui layout
